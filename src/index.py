9a18376e7dc50c9588e6977321e7cdb3b4ef5729
pietarni <pietari.niinimaki@helsinki.fi>
2024-11-09 12:40:22 +0200

    pylint fix

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
-    print("Luonnin jälkeen: sfdgmdfsklsadfffffffffffffffgödfjsglkfsdjgdflösgjksdfgfjdklg")
+    print("Luonnin jälkeen: ")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
